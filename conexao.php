9b724c69f07677fa03aaede80d80c446e8746551
Vinícius Marques Bezerra <viniciusmbpro@gmail.com>
2022-09-29 08:04:57 -0300

    c

--- conexao.php
+++ conexao.php
@@ -1,8 +1,8 @@
 <?php
-    $severname = "localhost:3306";
-    $username = "root";
-    $password = "admin";
-    $bdname = "chat";
+    $severname = "us-cdbr-east-06.cleardb.net";
+    $username = "b4e937b2de77ed";
+    $password = "bdd606a2";
+    $bdname = "heroku_40942db28c6b952";
 
     // create conection
     $conn = new mysqli($severname, $username, $password, $bdname);
